7b8de1f2b102e107fb879cf14301532cd06d9839
Marcos Santontos <marcoosppedro@gmail.com>
2026-03-30 23:41:32 -0300

    Adcionado modificações

---
+++ Exercícios/src/exercicio2/For.java
@@ -0,0 +1,13 @@
+package exercicio2;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Número: " + i);
+		}
+
+	}
+
+}
