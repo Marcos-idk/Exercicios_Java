bd9dc16603b45e0a7ffcc37e58f7d226a00e3d9d
Marcos Santontos <marcoosppedro@gmail.com>
2026-04-19 22:56:57 -0300

    Atualização

--- Exercícios/src/exercicio2/For.java
+++ Exercícios/src/exercicio2/For.java
@@ -1,13 +1,29 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class For {
 
 	public static void main(String[] args) {
-		
-		for (int i = 1; i <= 10; i++) {
-			System.out.println("Número: " + i);
-		}
+	Scanner sc = new Scanner (System.in);	
+	 int N = sc.nextInt();
+     int in = 0;
+     int out = 0;
+
+     for (int i = 0; i < N; i++) {
+         int X = sc.nextInt();
+
+         if (X >= 10 && X <= 20) {
+             in++;
+         } else {
+             out++;
+         }
+     }
+
+     System.out.println(in + " in");
+     System.out.println(out + " out");
+
+     sc.close();
 
 	}
-
 }
